9ade3b30422cde926535bf533a8e75f871c325e8
Ezequiel-Buzzella <ezequiel.buzzella@gmail.com>
2025-02-26 17:29:25 -0300

    Iniciando repositorio

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+import {src,dest,watch,series} from "gulp"
+import * as dartSass from "sass"
+import gulpSass from "gulp-sass"
+
+const sass= gulpSass(dartSass)
+export function js(done){
+    src('src/js/aapp.js')
+    .pipe(dest("dist/js"))
+
+    done()
+}
+
+export function css(done){
+    src("src/scss/app.scss",{sourcemaps:true})
+    .pipe(sass().on('error',sass.logError))
+    .pipe(dest("dist/css",{sourcemaps:true}))
+
+    done()
+}
+
+export function dev(){
+    watch("src/scss/**/*.scss",css)
+}
+
+export default series(js,css,dev)
